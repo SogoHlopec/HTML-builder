fb7728a8c4ed60de102ef1a9622ec33cd61b56d2
SogoHlopec <sogo246@gmail.com>
2022-11-08 04:59:07 +0300

    refactor: use async/await

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.promises.readdir(path.join(__dirname, "./secret-folder"), { withFileTypes: true }).then((files) => {
+async function read() {
+  const files = await fs.promises.readdir(path.join(__dirname, "./secret-folder"), { withFileTypes: true });
   for (const file of files) {
     if (file.isFile()) {
       const name = file.name;
       const filePath = path.join(__dirname, "./secret-folder", name);
       const extension = path.extname(name).slice(1);
       fs.stat(filePath, (err, stats) => {
-        console.log(stats.size);
-        console.log(`${name} - ${extension} - ${(stats.size/1024).toFixed(3)}kb`);
+        console.log(`${name} - ${extension} - ${(stats.size / 1024).toFixed(3)}kb`);
       });
     }
   }
-})
+}
+read();
 
 
 
