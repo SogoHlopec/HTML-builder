e83bf210d430b50ddacc2fe8e61722fbd7c03bf8
SogoHlopec <sogo246@gmail.com>
2022-11-08 04:24:21 +0300

    feat: add solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.promises.readdir(path.join(__dirname, "./secret-folder"), { withFileTypes: true }).then((files) => {
+  for (const file of files) {
+    if (file.isFile()) {
+      const name = file.name;
+      const filePath = path.join(__dirname, './secret-folder', name);
+      const extension = path.extname(name).slice(1);
+      fs.stat(filePath, (err, stats) => {
+        console.log(stats.size);
+        console.log(`${name} - ${extension} - ${(stats.size/1024).toFixed(3)}kb`);
+      });
+    }
+  }
+})
+
+
+
