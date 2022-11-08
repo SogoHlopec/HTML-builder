6e26cdeea020d988bc965f563df7d8748cbf8a4d
SogoHlopec <sogo246@gmail.com>
2022-11-08 06:06:09 +0300

    fix: changed deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,23 +3,17 @@ const path = require("path");
 
 async function copyDir() {
   try {
-    const filesCopy = await fs.promises.readdir(path.join(__dirname, "./files-copy"));
-    for (const file of filesCopy) {
-      fs.unlink(path.join(__dirname, "./files-copy", file), err => {
-        if (err) return console.error(err)
-      })
-    }
+    await fs.promises.rm(path.join(__dirname, "files-copy"), { recursive: true, force: true });
     await fs.promises.mkdir(path.join(__dirname, "./files-copy"), { recursive: true });
     const files = await fs.promises.readdir(path.join(__dirname, "files"));
-    console.log(files);
     for (const file of files) {
       const fileFrom = path.join(__dirname, "files", file);
       const fileIn = path.join(__dirname, "files-copy", file);
       await fs.promises.copyFile(fileFrom, fileIn);
     }
   }
-  catch (err) {
-    console.log(`Error: ${err.message}`);
+  catch (error) {
+    console.error(error.message);
   }
 }
 copyDir();
