9e8a963aeef6f303d3ac8d3d38926091144bc8a0
SogoHlopec <sogo246@gmail.com>
2022-11-08 06:10:59 +0300

    refactor: add output error to 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,16 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 async function read() {
-  const files = await fs.promises.readdir(path.join(__dirname, "./secret-folder"), { withFileTypes: true });
-  for (const file of files) {
-    if (file.isFile()) {
-      const name = file.name;
-      const filePath = path.join(__dirname, "./secret-folder", name);
-      const extension = path.extname(name).slice(1);
-      fs.stat(filePath, (err, stats) => {
-        console.log(`${name} - ${extension} - ${(stats.size / 1024).toFixed(3)}kb`);
-      });
+  try {
+    const files = await fs.promises.readdir(path.join(__dirname, "./secret-folder"), { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const name = file.name;
+        const filePath = path.join(__dirname, "./secret-folder", name);
+        const extension = path.extname(name).slice(1);
+        fs.stat(filePath, (err, stats) => {
+          console.log(`${name} - ${extension} - ${(stats.size / 1024).toFixed(3)}kb`);
+        });
+      }
     }
+  }
+  catch (error) {
+    console.error(error.message);
   }
 }
 read();
